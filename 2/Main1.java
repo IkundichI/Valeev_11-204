286e002991b68fefb8d46771c93d13436fcf1538
Valeev Artem <52968778+IkundichI@users.noreply.github.com>
2022-09-15 22:11:10 +0300

    add 2nd lesson

---
+++ 2/Main1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main1 {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int x = scanner.nextInt();
+	int y = scanner.nextInt();
+	
+	x = x * y;
+	y = x / y;
+	x = x / y;
+
+	System.out.println(x);
+	System.out.println(y);
+
+	}
+}

4a3b3e92d068664de7f7a89c1a7332f1c582590c
Valeev Artem <52968778+IkundichI@users.noreply.github.com>
2023-03-18 18:10:14 +0300

    add hw4

---
+++ hw4/MySet.java
@@ -0,0 +1,129 @@
+package MySet;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class MySet<T> extends AbstractSet<T> implements SortedSet<T> {
+
+	private T[] elements;
+	private Comparator<? super T> comparator;
+	private T current;
+
+	public MySet(Collection<? extends T> c, Comparator<? super T> comparator, Class<T> tClass) {
+		this.comparator = comparator;
+		this.elements = (T[]) Array.newInstance(tClass, c.size());
+		c.toArray(this.elements);
+		Arrays.sort(this.elements, comparator);
+	}
+	public MySet(Comparator<? super T> comparator, Class<T> tClass) {
+		this.comparator = comparator;
+		this.elements = (T[]) Array.newInstance(tClass, 0);
+	}
+
+
+	public boolean add(T e) {
+		if (e == null) {
+			return false;
+		}
+		for (int i = 0; i < elements.length; i++) {
+			if (elements[i].equals(e)) {
+				return false;
+			}
+		}
+		T[] newElements = (T[]) Array.newInstance(elements.getClass().getComponentType(), this.size() + 1);
+		for (int i = 0; i < elements.length; i++) {
+			newElements[i] = elements[i];
+		}
+		int count = size();
+		newElements[count] = e;
+		elements = newElements;
+		Arrays.sort(elements, comparator);
+
+
+		return true;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			private int index = 0;
+
+			@Override
+			public boolean hasNext() {
+				return index < size();
+			}
+
+			@Override
+			public T next() {
+				if (!hasNext()) {
+					throw new NoSuchElementException();
+				}
+				T element = elements[index];
+				index++;
+				return element;
+			}
+		};
+	}
+
+	@Override
+	public int size() {
+		return this.elements.length;
+	}
+
+	@Override
+	public Comparator<? super T> comparator() {
+		return this.comparator;
+	}
+
+	@Override
+	public SortedSet<T> subSet(T fromElement, T toElement) {
+		int fromIndex = Arrays.binarySearch(this.elements, fromElement, comparator);
+		int toIndex = Arrays.binarySearch(this.elements, toElement, comparator);
+		if (fromIndex < 0) {
+			fromIndex = -fromIndex - 1;
+		}
+		if (toIndex < 0) {
+			toIndex = -toIndex - 1;
+		}
+		return new MySet<>(Arrays.asList(elements).subList(fromIndex, toIndex), comparator(),
+				  (Class<T>) elements.getClass().getComponentType());
+	}
+
+	@Override
+	public SortedSet<T> headSet(T toElement) {
+		MySet<T> newMySet = new MySet<T>(comparator, (Class<T>) elements.getClass().getComponentType());
+		for (int i = 0; i < elements.length; i++) {
+			if (comparator.compare(elements[i], toElement) < 0) {
+				newMySet.add(elements[i]);
+			}
+		}
+		return newMySet;
+	}
+
+	@Override
+	public SortedSet<T> tailSet(T fromElement) {
+		MySet<T> newMySet = new MySet<T>(comparator, (Class<T>) elements.getClass().getComponentType());
+		for (int i = 0; i < elements.length; i++) {
+			if (comparator.compare(elements[i], fromElement) >= 0) {
+				newMySet.add(elements[i]);
+			}
+		}
+		return newMySet;
+	}
+
+	@Override
+	public T first() {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return elements[0];
+	}
+
+	@Override
+	public T last() {
+		if (size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return elements[size() - 1];
+	}
+}
